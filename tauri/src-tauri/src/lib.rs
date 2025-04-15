b538e00b667a0b12667031a685a24ef290fe5aa6
andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>
2025-04-15 11:00:49 +0200

    fix: remove tauri sql plugin - this was causing too many issues
    this plugin broke cargo tauri dev and cargo checks because of some
    issues with wasm support. I tried to apply the suggested fixes to no
    avail. removing it for now, until there actually is a rust api to
    peruse.
    
    suggested fix:
    https://docs.rs/getrandom/latest/getrandom/#webassembly-support
    a github issue common to this problem:
    https://github.com/tauri-apps/plugins-workspace/issues/506
    
    Signed-off-by: andreacfromtheapp <3269984+andreacfromtheapp@users.noreply.github.com>

--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -87,7 +87,6 @@ async fn get_feed(app_handle: tauri::AppHandle, account: Account, sub_link: Subs
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
